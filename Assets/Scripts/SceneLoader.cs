652bb81ea8fc6795678df4e5663c8fe42c721656
Kris <kris.lafrance6067@gmail.com>
2021-05-16 23:36:02 -0700

    Initial Commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadMenuScreen(string menuOption)
+    {
+        SceneManager.LoadScene(menuOption);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
