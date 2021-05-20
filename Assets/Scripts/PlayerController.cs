aa2f1bb946612c2fd3b7d1120ac9472a538111be
Kris <kris.lafrance6067@gmail.com>
2021-05-20 01:48:49 -0700

    Added movement limiter on both control types

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMin = 1, xMax = 15;
 
     [SerializeField] bool mouseIsActive = false;
     [SerializeField] bool controllerIsActive = true;
@@ -25,23 +26,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mouseIsActive)
-        {
-        float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
-        transform.position = paddlePos;
-
-        }
-        if (controllerIsActive)
-        {
-            rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
-            
-        }
+        Move();
     }
 
+    public void Move()
+    {
+        if (mouseIsActive)
+        {
+            float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+            Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+            paddlePos.x = Mathf.Clamp(mousePos, xMin, xMax);
+
+            transform.position = paddlePos;
+
+        }
+        else if (controllerIsActive)
+        {
+            rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+            Vector2 clampedPos = transform.position;
+            clampedPos.x = Mathf.Clamp(clampedPos.x, xMin, xMax);
+            transform.position = clampedPos;
+
+        }
+    }
     public void OnMovement(InputAction.CallbackContext context)
     {
         inputX = context.ReadValue<Vector2>().x;
-        Debug.Log("Controller");
     }
 }
